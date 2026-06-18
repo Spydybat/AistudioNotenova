2a504125e05a5db8f7a6a6ef391eb70b6ee26898
Kalaiyarasan <spydybat@gmail.com>
2026-06-18 12:55:12 +0200

    Fix Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/AistudioNotenova/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
